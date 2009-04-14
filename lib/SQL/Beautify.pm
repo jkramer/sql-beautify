ac337d81fa8e91bd68a6147083d990d55cae8e61
Jonas Kramer <jkramer@nex.scrapping.cc>
2009-04-15 00:44:40 +0200

    Support for custom rules.

--- lib/SQL/Beautify.pm
+++ lib/SQL/Beautify.pm
@@ -30,6 +30,7 @@ sub new {
 	$self->{break}    = "\n" unless defined($self->{break});
 	$self->{wrap}     = {}   unless defined($self->{wrap});
 	$self->{keywords} = []   unless defined($self->{keywords});
+	$self->{rules}    = {}   unless defined($self->{rules});
 
 	push @{$self->{keywords}}, KEYWORDS;
 
@@ -40,6 +41,7 @@ sub new {
 }
 
 
+# Add more SQL.
 sub add {
 	my ($self, $addendum) = @_;
 
@@ -49,6 +51,7 @@ sub add {
 }
 
 
+# Set SQL to beautify.
 sub query {
 	my ($self, $query) = @_;
 
@@ -58,6 +61,7 @@ sub query {
 }
 
 
+# Beautify SQL.
 sub beautify {
 	my ($self) = @_;
 
@@ -70,7 +74,14 @@ sub beautify {
 	$self->{_tokens} = [ SQL::Tokenizer->tokenize($self->query, 1) ];
 
 	while(defined(my $token = $self->_token)) {
-		if($token eq '(') {
+		my $rule = $self->_get_rule($token);
+		
+		# Allow custom rules to override defaults.
+		if($rule) {
+			$self->_process_rule($rule, $token);
+		}
+
+		elsif($token eq '(') {
 			$self->_add_token($token);
 			$self->_new_line;
 			push @{$self->{_level_stack}}, $self->{_level};
@@ -264,6 +275,50 @@ sub add_keywords {
 }
 
 
+# Add new rules.
+sub add_rule {
+	my ($self, $format, $token) = @_;
+
+	my $rules = $self->{rules}    ||= {};
+	my $group = $rules->{$format} ||= [];
+
+	push @{$group}, ref($token) ? @{$token} : $token;
+}
+
+
+# Find custom rule for a token.
+sub _get_rule {
+	my ($self, $token) = @_;
+
+	values %{$self->{rules}}; # Reset iterator.
+
+	while(my ($rule, $list) = each %{$self->{rules}}) {
+		return $rule if(grep { uc($token) eq uc($_) } @$list);
+	}
+
+	return undef;
+}
+
+
+sub _process_rule {
+	my ($self, $rule, $token) = @_;
+
+	my $format = {
+		break => sub { $self->_new_line                                     },
+		over  => sub { $self->_over                                         },
+		back  => sub { $self->_back                                         },
+		token => sub { $self->_add_token($token)                            },
+		push  => sub { push @{$self->{_level_stack}}, $self->{_level}       },
+		pop   => sub { $self->{_level} = pop(@{$self->{_level_stack}}) || 0 },
+		reset => sub { $self->{_level} = 0; @{$self->{_level_stack}} = ();  }, 
+	};
+
+	for(split /-/, lc $rule) {
+		&{$format->{$_}} if($format->{$_});
+	}
+}
+
+
 # Check if a token is a constant.
 sub _is_constant {
 	my ($self, $token) = @_;
@@ -354,6 +409,49 @@ Beautifies the internally saved SQL string and returns the result.
 =item B<add_keywords>($keyword, $another_keyword, \@more_keywords)
 
 Add any amount of keywords of arrays of keywords to highlight.
+
+=item B<add_rule>($rule, $token)
+
+Add a custom formatting rule. The first argument is the rule, a string
+containing one or more commands (explained below), separated by dashes. The
+second argument may be either a token (string) or a list of strings. Tokens are
+grouped by rules internally, so you may call this method multiple times with
+the same rule string and different tokens, and the rule will apply to all of
+the tokens.
+
+The following formatting commands are known at the moment:
+
+=over 4
+
+=item B<token> - insert the token this rule applies to
+
+=item B<over> - increase indentation level
+
+=item B<back> - decrease indentation level
+
+=item B<break> - insert line break
+
+=item B<push> - push current indentation level to an internal stack
+
+=item B<pop> - restore last indentation level from the stack
+
+=item B<reset> - reset internal indentation level stack
+
+=back
+
+B<push>, B<pop> and B<reset> should be rarely needed.
+
+
+B<NOTE>:
+Custom rules override default rules. Some default rules do things that
+can't be done using custom rules, such as changing the format of a token
+depending on the last or next token.
+
+
+B<NOTE>:
+I'm trying to provide sane default rules. If you find that a custom
+rule of yours would make more sense as a default rule, please create a ticket.
+
 
 =back
 
